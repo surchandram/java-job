c4fa3283637ff37cb331cbde7cfdda8c0666b654
surchandram <surchandram.com>
2026-01-29 19:23:52 +0530

    first commit

---
+++ src/main/java/com/surchandra/JobApp/controller/JobController.java
@@ -0,0 +1,41 @@
+package com.surchandra.JobApp.controller;
+
+import com.surchandra.JobApp.model.JobPost;
+import com.surchandra.JobApp.service.JobService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class JobController {
+
+    @Autowired
+    private JobService service;
+
+    @GetMapping({"/", "home"})
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("addjob")
+    public String addJob() {
+        return "addjob";
+    }
+
+    @PostMapping("handleForm")
+    public String handleForm(JobPost jobPost) {
+        service.addJob(jobPost);
+        return "success";
+    }
+
+    @GetMapping("viewalljobs")
+    public String viewJobs(Model model) {
+        List<JobPost> jobs = service.getAllJobs();
+        model.addAttribute("jobPosts", jobs);
+        return "viewalljobs";
+    }
+}
